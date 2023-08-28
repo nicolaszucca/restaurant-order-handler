36c8482c0faf5a155ce61830fa233c88adb579ce
Nicolas Zuccarello <zuccarelo1@gmail.com>
2023-08-28 16:59:18 -0300

    Add: central & change data structure of tables & add function to bettter handle the app logic

--- models/tables-control.js
+++ models/tables-control.js
@@ -2,15 +2,23 @@ const path = require('path');
 const fs = require('fs');
 
 class Table {
-	constructor(number, order = {}) {
-		this.number = number;
+	constructor(name, order = {}) {
+		this.name = name;
 		this.order = order;
+		this.price = 0;
+		this.alive = true;
+	}
+}
+class Central {
+	constructor(name = '') {
+		this.name = name;
 	}
 }
 
 class TableControl {
 	constructor() {
-		this.tables = [];
+		this.tables = {};
+		this.central = {};
 		this.date = new Date().getDate();
 
 		this.init();
@@ -33,14 +41,23 @@ class TableControl {
 	}
 
 	get tablesArr() {
-		return this.tables;
+		return Object.values(this.tables);
+
 	}
 	saveDb() {
 		const dbPath = path.join(__dirname, '../DB/db.json');
 		fs.writeFileSync(dbPath, JSON.stringify(this.toJSON));
 	}
-	newTable(number, order) {
-		this.tables.push(new Table(number, order));
+	newTable(name, order) {
+		this.tables[name] = new Table(name, order);
+	}
+
+	logInCentral(name) {
+		this.central[name] = new Central(name);
+	}
+
+	disconnectTable(name) {
+		delete this.tables[name];
 	}
 }
 
