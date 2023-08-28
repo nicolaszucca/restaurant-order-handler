92a9d0d9435d20f869b7ab4f75d1bd7f895abe3e
Nicolas Zuccarello <zuccarelo1@gmail.com>
2023-08-28 16:45:42 -0300

    Simplification of the 'validate' function using 'validationResult' exclusively

--- middlewares/valid-fields.js
+++ middlewares/valid-fields.js
@@ -1,22 +1,13 @@
 const { validationResult } = require('express-validator');
 
 
-const validate = validations => {
-	return async (req, res, next) => {
-		for (let validation of validations) {
-			const result = await validation.run(req);
-			if (result.errors.length) break;
-		}
-
-		const errors = validationResult(req);
-		if (errors.isEmpty()) {
-			return next();
-		}
-
-		res.status(400).json({ errors: errors.array() });
-	};
+const validate = (req, res, next) => {
+	const result = validationResult(req);
+	if (!result.isEmpty()) {
+		return res.send({ errors: result.array() });
+	}
+	return next();
 };
-
 
 module.exports = {
 	validate,
